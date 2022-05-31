8cedbc2ea279a8633b0eb0d4f1745115f2ae9290
Chetan Marathe <ckmarathe0326@gmail.com>
2022-05-31 23:52:03 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,15 +11,3 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
-
-provider "kubectl" {
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
-}
